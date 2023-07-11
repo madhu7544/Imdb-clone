f8102e13f291266a47ad56aeed3a0a1298e0341c
madhu7544 <sahukari.kumar.24.1@mountblue.tech>
2023-07-11 17:59:24 +0530

    added actors model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_122237) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_122713) do
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "director"
