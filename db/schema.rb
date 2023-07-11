1153da0240e2da151e1ae0bf447a38eb53f6da94
madhu7544 <sahukari.kumar.24.1@mountblue.tech>
2023-07-11 17:53:17 +0530

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_113610) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_122237) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "director"
@@ -23,6 +23,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_113610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "poster"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
