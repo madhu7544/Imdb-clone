9d47bd9de39e98e22a82a353ae6671daec36bcd7
madhu7544 <sahukari.kumar.24.1@mountblue.tech>
2023-07-21 15:08:01 +0530

    added debouncing in home search

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,69 +1,75 @@
 let searchForm = document.getElementById("search-form");
 let selectOption = document.getElementById("selectOption");
 
+let debounceTimeoutId;
+
 searchForm.addEventListener("input", function (event) {
   event.preventDefault();
 
-  let search = "";
-  const query = document.getElementById("query").value.trim();
-  if (query.length > 3) {
-    search = query;
-  }
+  clearTimeout(debounceTimeoutId);
 
-  let option = selectOption.value;
-  let rating = "";
-  let all = "";
-  if (option == "rating") {
-    rating = "rating";
-  } else if (option == "all") {
-    all = "all";
-  }
+  debounceTimeoutId = setTimeout(() => {
+    let search = "";
+    const query = document.getElementById("query").value.trim();
+    if (query.length > 3) {
+      search = query;
+    }
 
-  const selectGenre = document.getElementById("selectGenre").value;
-  const selectYear = document.getElementById("selectYear").value;
+    let option = selectOption.value;
+    let rating = "";
+    let all = "";
+    if (option == "rating") {
+      rating = "rating";
+    } else if (option == "all") {
+      all = "all";
+    }
+
+    const selectGenre = document.getElementById("selectGenre").value;
+    const selectYear = document.getElementById("selectYear").value;
+    const formData = {
+      title: search,
+      genre: selectGenre,
+      year: selectYear,
+      rating: rating,
+      all: all,
+    };
+
+    fetchMoviesAfter(formData);
+  }, 700);
+});
+
+const fetchMoviesAfter = (formData) => {
   const moviesPage = document.getElementById("moviesPage");
-  const formData = {
-    title: search,
-    genre: selectGenre,
-    year: selectYear,
-    rating: rating,
-    all: all,
-  };
-
-  setTimeout(() => {
-    let formData2 = new URLSearchParams(formData);
-    fetchMoviesAfter(formData2);
-  }, 500);
-
-  const fetchMoviesAfter = (formData2) => {
-    fetch(`/search?${formData2}`, {
-      method: "GET",
-      headers: {},
+  let formData2 = new URLSearchParams(formData);
+  fetch(`/search?${formData2}`, {
+    method: "GET",
+    headers: {},
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        moviesPage.innerHTML = "";
-        data.map((movie) => {
-          moviesPage.innerHTML += `
+    .then((data) => {
+      moviesPage.innerHTML = "";
+      data.map((movie) => {
+        moviesPage.innerHTML += `
             <li class="each-movie">
               <a  href="/movies/${movie.id}" class="movie-anchor">
                 <img src="${movie.poster}" class="movie-image"/>
                 <div class="content">
                   <h3 class="movie-title">${movie.title}</h3>
-                  <p class="movie-rating">Ratings: ${Math.round(movie.average * 10) / 10}</p>
+                  <p class="movie-rating">Ratings: ${
+                    Math.round(movie.average * 10) / 10
+                  }</p>
                 </div>
               </a>
             </li>
           `;
-        });
-      })
-      .catch((error) => {
-        console.log(error);
       });
-  };
-});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 selectOption.addEventListener("change", () => {
   const selectedOption = selectOption.value;
@@ -75,7 +81,6 @@ selectOption.addEventListener("change", () => {
   } else if (selectedOption === "release-year") {
     genreFilter.style.display = "none";
     yearFilter.style.display = "block";
-    
   } else {
     genreFilter.style.display = "none";
     yearFilter.style.display = "none";
